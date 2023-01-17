8527a3abb7845d551c79e8db9e57dc5040e9a4ac
jeppelind <58559776+jeppelind@users.noreply.github.com>
2023-01-17 13:18:40 +0100

    Update workflow.

--- client/src/features/event/Events.tsx
+++ client/src/features/event/Events.tsx
@@ -14,8 +14,9 @@ const fetchEvents = async (startIndex: number, endIndex: number) => {
       }
     }`;
   try {
+    const url = process.env.REACT_APP_API_URL;
     const token = process.env.REACT_APP_AUTH_TOKEN || '';
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/graphql`, {
+    const response = await fetch(`${url}}/graphql`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
